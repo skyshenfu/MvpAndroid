67beb0d372889c7a25df64fd3d1986c29067583b
painworkpc <zhangty@all-help.com>
2017-02-28 09:38:13 +0800

    增加屏幕适配文件

---
+++ Wetest/app/src/main/java/com/lude/wetest/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.lude.wetest;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by zhangty on 2017/2/28.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
